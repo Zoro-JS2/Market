4640edf02e543cf2c3fd975bc3fc7ccaf9026c0e
Rigidbody-JS <66162438+Zoro-JS2@users.noreply.github.com>
2020-11-14 14:28:29 +0200

    Add files via upload

---
+++ NM.js
@@ -0,0 +1,11 @@
+function theme(){
+let mode=document.querySelector("#mode");
+let them=false;
+if(them==false){
+    document.body.classList.toggle("dark")
+}
+else{
+    document.body.classList.toggle("wight")
+
+}
+}
